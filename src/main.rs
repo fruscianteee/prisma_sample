5a183c510dce1f1ca6b65b5744fb37a59f1be8c0
fruscianteee <redhot1205@gmail.com>
2024-03-14 01:41:48 +0900

    スキーマを定義、mysqlの準備など

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+#[allow(warnings, unused)]
+mod prisma;
+
+use anyhow::Result;
+use prisma::PrismaClient;
+use prisma_client_rust::NewClientError;
+use tracing::info;
+
+use crate::prisma::post;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenvy::dotenv().expect(".env file not found");
+    dbg!(std::env::var("DATABASE_URL").unwrap());
+    let client = PrismaClient::_builder()
+        .with_url(std::env::var("DATABASE_URL").expect("No DATABASE_URL environment variable"))
+        .build()
+        .await?;
+
+    // DB同期
+    #[cfg(debug_assertions)]
+    client._db_push().await?;
+    // ここがちゃんと動作しているかは不明
+    #[cfg(not(debug_assertions))]
+    client._migrate_deploy().await?;
+
+    // let a = client
+    //     .user()
+    //     .create("id".to_string(), "display_name".to_string(), vec![])
+    //     .exec()
+    //     .await?;
+
+    dbg!(client.user().find_many(vec![]).exec().await);
+    // dbg!("db push");
+    // let a = client._db_push().await.unwrap();
+    // dbg!(a);
+    // dbg!("Running database migrations");
+    // client._migrate_deploy().await?;
+    // dbg!("Database migrations completed");
+
+    // let a = client.user().find_many(vec![]).exec().await;
+    // dbg!(a);
+    Ok(())
 }
